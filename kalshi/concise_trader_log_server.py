a152abd799e5ca1717fa0b0dae4434b93c471d57
Joe on WSL <chengyou1368@gmail.com>
2026-05-18 09:23:37 -0400

    fixed concise server v2

--- kalshi/concise_trader_log_server.py
+++ kalshi/concise_trader_log_server.py
@@ -186,4 +186,4 @@ def log() -> Response:
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8087, debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=8086, debug=False, threaded=True)
